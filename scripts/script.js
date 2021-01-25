b4a303111b542309b47cb8463fa8e799c9dd334d
stephenPC <stephenmissah@outlook.com>
2021-01-25 14:57:26 +0000

    resolved showcase image duplicate

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,29 @@
 const navCollapsed = document.querySelector(".collapsed");
 const navOpen = document.querySelector(".open");
-
 const menuOverlay = document.querySelector(".menu-overlay");
 
+//!when the tacked menu is clicked
 navCollapsed.addEventListener("click", () => {
   navCollapsed.classList.toggle("hidden");
   navOpen.classList.toggle("hidden");
   menuOverlay.classList.toggle("active");
   //   menuOverlay.classList.toggle("inactive");
 });
+
+//! when the x button is close
 navOpen.addEventListener("click", () => {
   navCollapsed.classList.toggle("hidden");
   navOpen.classList.toggle("hidden");
   menuOverlay.classList.toggle("active");
   //   menuOverlay.classList.toggle("inactive");
+});
+
+//! when mouse leaves the open menu
+menuOverlay.addEventListener("mouseleave", () => {
+  //   alert("menu test out");
+
+  //!use MOUSELEAVE NOT MOUSE OUT - out triggers even when i hover over text within the menu
+  navCollapsed.classList.toggle("hidden");
+  navOpen.classList.toggle("hidden");
+  menuOverlay.classList.remove("active");
 });
